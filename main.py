f133ad94585d9040be63d6deb883952b964d9101
Talon32 <icepulse12@gmail.com>
2025-01-09 22:12:42 -0700

    Finished Project

--- main.py
+++ main.py
@@ -11,18 +11,32 @@ def word_count(text):
     count = 0
     for word in words_split:
          count += 1
+    print("========= Start of Report =========")
     print(f"{count} words found in the document")
 
 def char_count(text):
     text_lowered = text.lower()
     alpha_dict = {}
+    char_list = []
 
     for char in text_lowered:
         if char in alpha_dict:
             alpha_dict[char] += 1
         else:
             alpha_dict[char] = 1
-    return alpha_dict
+    #return alpha_dict
+    for char in alpha_dict:
+        if char.isalpha():
+            char_dict = {"char": char, "num": alpha_dict[char]}
+            char_list.append(char_dict)
+            
+    def sort_dict(dict):
+        return dict["num"]
+    
+    char_list.sort(reverse=True, key=sort_dict)
+    for item in char_list:
+        print(f"The {item["char"]} character was found {item["num"]} times.")
+    print("========= End of Report =========")
 
             
 main()
